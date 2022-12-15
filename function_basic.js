d2c3f1eea76afdb3e8ff31bddbc33fbbc175b0c3
kamala-github <agnes.kamaladevi@gmail.com>
2022-12-14 22:23:29 -0500

    My First Commit

---
+++ function_basic.js
@@ -0,0 +1,10 @@
+function f ()
+{
+    console.log("function f")
+
+    if (4 < 2) console.log("yes")
+    else console.log('no')
+}
+
+
+f();
---
+++ js.basic.js
@@ -0,0 +1,15 @@
+var nam = 'Adam' //string
+nam= 'Eve'
+var age = 945 //number
+var email = 'kamalaedison'
+var address = '1163 ba'
+var sum = 20+30 //50
+var x= 'a'+10 //a10
+var b= 'hi'+'all' //hi all
+var isHuman = true//boolen var(either true or false)
+// isHuman= false
+var isvaccinated=false
+var x=1
+var y=2
+var z= x+y
+console.log(z)
